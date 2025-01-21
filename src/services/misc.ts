311160f0097c31a02058cb6625daf8c88bdde927
WiseyAI <aryan@explainx.ai>
2025-01-21 17:32:35 +0530

    fix : name regex

--- src/services/misc.ts
+++ src/services/misc.ts
@@ -2,8 +2,8 @@ export function cleanUpAuthorName(text: string) {
   if (!text) {
     return "";
   }
-  const regex = /^(.+?)(?:'s\s+Post|'s Post|Post)/;
+  const regex = /^(.+?)(?:'s\s*Post|Post)/;
   const match = text.match(regex);
-  const name = match ? match[1].trim() : "";
+  const name = match ? match[1].trim().replace(/'s$/, "") : "";
   return name;
 }
